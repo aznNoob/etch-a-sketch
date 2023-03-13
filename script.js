b8d924037bb9e1a875e58795228343d035520962
aznNoob <nubdotes@gmail.com>
2023-03-14 00:17:06 +0930

    Tidy up and added clear button to the code and minimal styling changes

--- script.js
+++ script.js
@@ -1,41 +1,53 @@
+const defaultSize = 16;
+const body = document.querySelector('body');
 
+let mouseDown = false;
 
+document.body.addEventListener('mousedown', function () {
+    mouseDown = true;
+})
+
+document.body.addEventListener('mouseup', function () {
+    mouseDown = false;
+})
 
 const createGrid = (numberRowsColumns) => {
+    const container = document.createElement('div');
+    container.classList.toggle('container');
+    container.style.gridTemplateColumns = `repeat(${numberRowsColumns}, 1fr)`;
+    body.appendChild(container);
+
     for (let i = 0; i < (numberRowsColumns * numberRowsColumns); i++) {
         const div = document.createElement('div');
         div.style.backgroundColor = 'white';
         div.classList.toggle('square');
+        div.addEventListener("mouseover", function (e) {
+            if (mouseDown) {
+                e.target.style.backgroundColor = '#ff5e6c';
+            }
+        });
         container.appendChild(div);
-        // container.style.gridTemplateRows = `repeat(${numberRowsColumns}, 30px[col - start])`
-        container.style.gridTemplateColumns = `repeat(${numberRowsColumns}, 1fr)`;
     }
 };
 
+function createButtons () {
+    const div = document.createElement('div');
+    div.setAttribute("class", "buttons-container");
+    body.appendChild(div);
+    
+    const clearBtn = document.createElement('button');
+    clearBtn.textContent = 'Clear';
+    clearBtn.classList.toggle('clear-button');
+    div.appendChild(clearBtn);
+    clearBtn.addEventListener("click", clearGrid)
+}
+
+function clearGrid () {
+    const square = document.querySelectorAll('.square');
+    square.forEach(square => square.style.backgroundColor = 'white');
+}
+
+createGrid(defaultSize);
+createButtons();
 
 
-let mouseDown = false;
-const body = document.querySelector('body');
-
-const container = document.createElement('div');
-container.classList.toggle('container');
-
-body.appendChild(container);
-
-createGrid(16);
-
-const square = document.querySelectorAll('.square');
-
-square.forEach(square => square.addEventListener("mouseover", function (e) {
-    if (mouseDown) {
-        e.target.style.backgroundColor = '#ff5e6c';
-    }
-}))
-
-document.addEventListener('mousedown', function () {
-    mouseDown = true;
-})
-
-document.addEventListener('mouseup', function () {
-    mouseDown = false;
-})
